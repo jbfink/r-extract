67e4f517a798546c38eac13c519f92b632b5fe82
jbfink <john.fink@gmail.com>
2022-09-06 16:35:15 -0400

    csv file reads and prints to stdout

--- zhou-test.py
+++ zhou-test.py
@@ -1,4 +1,5 @@
 import praw
+import csv
 
 # print("Dude")
 
@@ -10,3 +11,8 @@ reddit = praw.Reddit("zhou-rscrape",
 subreddit = reddit.subreddit("taskmaster")
 print(subreddit.title)
 
+filename = 'url-list.csv'
+with open(filename, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        print(row)
