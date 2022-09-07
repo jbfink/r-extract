eda2b3427eb1c5f230586e76c8bbc12afb7873e7
jbfink <john.fink@gmail.com>
2022-09-07 11:05:45 -0400

    pandas to export to a csv.

--- small-test.py
+++ small-test.py
@@ -1,5 +1,7 @@
 import praw
 import csv
+import pandas as pd
+import datetime as dt
 
 # print("Dude")
 
@@ -11,7 +13,12 @@ reddit = praw.Reddit("zhou-rscrape",
 url = 'https://old.reddit.com/r/taskmaster/comments/x7g3le/last_tmnz_episode_today_who_do_you_guys_think/'
 
 submission = reddit.submission(url=url)
-submission.comments.replace_more(limit=None)
-for comment in submission.comments.list():
-    print(comment.body)
+comments = submission.comments
+df_rows = [[comment.author, comment.parent, comment.id, comment.score, comment.created, comment.body] for comment in comments]
+df = pd.DataFrame(df_rows, columns=['Author','Parent ID', 'Comment ID', 'Score', 'Created', 'Body'])
+df.to_csv('small.csv')
+
+#submission.comments.replace_more(limit=None)
+#for comment in submission.comments.list():
+#    print(comment.body)
 
