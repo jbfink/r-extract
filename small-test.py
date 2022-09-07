40a1524a7d98d8e3f7f8ca35b90be6d1af564e6e
jbfink <john.fink@gmail.com>
2022-09-07 11:34:05 -0400

    tinkering with small-test, removing small.csv

--- small-test.py
+++ small-test.py
@@ -10,12 +10,13 @@ reddit = praw.Reddit("zhou-rscrape",
          user_agent="zhou-rscrape"
 )
 
-url = 'https://old.reddit.com/r/taskmaster/comments/x7g3le/last_tmnz_episode_today_who_do_you_guys_think/'
+#url = 'https://old.reddit.com/r/taskmaster/comments/x7g3le/last_tmnz_episode_today_who_do_you_guys_think/'
+url = 'https://www.reddit.com/r/antiwork/comments/pdeevi/chinese_millennials_are_choosing_slower_lives_and/'
 
 submission = reddit.submission(url=url)
 comments = submission.comments
-df_rows = [[comment.author, comment.parent, comment.id, comment.score, comment.created, comment.body] for comment in comments]
-df = pd.DataFrame(df_rows, columns=['Author','Parent ID', 'Comment ID', 'Score', 'Created', 'Body'])
+df_rows = [[comment.author, comment.id, comment.score, comment.body, comment.replies] for comment in comments]
+df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', "Replies"])
 df.to_csv('small.csv')
 
 #submission.comments.replace_more(limit=None)
