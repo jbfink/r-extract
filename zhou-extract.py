b72f5395ab04783c0eee762dad8baba73fe3a646
jbfink <john.fink@gmail.com>
2022-09-08 12:39:25 -0400

    Finished, I think. Some explanatory comments.

--- zhou-extract.py
+++ zhou-extract.py
@@ -14,8 +14,10 @@ urls = 'url-list.csv'
 with open(urls, 'r') as csvfile:
     datareader = list(csv.reader(csvfile, delimiter = ","))
     datareader = datareader[0]
+    # this is needed because the list returned is a *nested* list, viz, one that is [[ <blah, blah, blah> ]], and just trying to access it directly means you get the *one* element of the original list. Using [0] effectively strips out the extra brackets.
     number = 1
     for row in datareader:
+        print(number)
         submission = reddit.submission(url=row)
         submission.comments.replace_more(limit=None)
         comments = submission.comments.list()
@@ -23,16 +25,8 @@ with open(urls, 'r') as csvfile:
         df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
         df.to_csv(f'file{number}.csv')
         number = number + 1
-        print(number)
 
  
 
-#url = 'https://www.reddit.com/r/antiwork/comments/pdeevi/chinese_millennials_are_choosing_slower_lives_and/'
-
-#submission = reddit.submission(url=url)
-#comments = submission.comments.list()
-#df_rows = [[comment.author, comment.id, comment.score, comment.body] for comment in comments]
-#df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
-#df.to_csv('small.csv')
 
 
