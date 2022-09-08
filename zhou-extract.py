4628532426055edbbdc7d9ae740c3741f6de4988
jbfink <john.fink@gmail.com>
2022-09-08 11:43:40 -0400

    Broke, of course.

--- zhou-extract.py
+++ zhou-extract.py
@@ -3,19 +3,31 @@ import csv
 import pandas as pd
 import datetime as dt
 
-# print("Dude")
 
 
 reddit = praw.Reddit("zhou-rscrape",
          user_agent="zhou-rscrape"
 )
 
-url = 'https://www.reddit.com/r/antiwork/comments/pdeevi/chinese_millennials_are_choosing_slower_lives_and/'
+urls = 'url-list.csv'
 
-submission = reddit.submission(url=url)
-comments = submission.comments.list()
-df_rows = [[comment.author, comment.id, comment.score, comment.body] for comment in comments]
-df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
-df.to_csv('small.csv')
+with open(urls, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        submission = reddit.submission(url=row)
+        comments = submission.comments.list()
+        df_rows = [[comment.author, comment.id, comment.score, comment.body] for comment in comments]
+        df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
+        df.to_csv('notgonnawork.csv')
+
+ 
+
+#url = 'https://www.reddit.com/r/antiwork/comments/pdeevi/chinese_millennials_are_choosing_slower_lives_and/'
+
+#submission = reddit.submission(url=url)
+#comments = submission.comments.list()
+#df_rows = [[comment.author, comment.id, comment.score, comment.body] for comment in comments]
+#df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
+#df.to_csv('small.csv')
 
 
