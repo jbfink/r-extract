38b8dab06c368507a1db88cf9927e216d55128b6
jbfink <john.fink@gmail.com>
2022-09-07 22:22:52 -0400

    CommentForest removed from pd, .list() added to comments to capture both comments and replies.

--- small-test.py
+++ small-test.py
@@ -14,9 +14,9 @@ reddit = praw.Reddit("zhou-rscrape",
 url = 'https://www.reddit.com/r/antiwork/comments/pdeevi/chinese_millennials_are_choosing_slower_lives_and/'
 
 submission = reddit.submission(url=url)
-comments = submission.comments
-df_rows = [[comment.author, comment.id, comment.score, comment.body, comment.replies] for comment in comments]
-df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', "Replies"])
+comments = submission.comments.list()
+df_rows = [[comment.author, comment.id, comment.score, comment.body] for comment in comments]
+df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
 df.to_csv('small.csv')
 
 #submission.comments.replace_more(limit=None)
