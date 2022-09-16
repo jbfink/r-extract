1ef56317be9bcde4bbc7a071bc1138a155837609
jbfink <john.fink@gmail.com>
2022-09-16 12:20:43 -0400

    used black to reformat zhou-reddit.

--- zhou-extract.py
+++ zhou-extract.py
@@ -3,15 +3,12 @@ import csv
 import pandas as pd
 
 
+reddit = praw.Reddit("zhou-rscrape", user_agent="zhou-rscrape")
 
-reddit = praw.Reddit("zhou-rscrape",
-         user_agent="zhou-rscrape"
-)
+urls = "url-list.csv"
 
-urls = 'url-list.csv'
-
-with open(urls, 'r') as csvfile:
-    datareader = list(csv.reader(csvfile, delimiter = ","))
+with open(urls, "r") as csvfile:
+    datareader = list(csv.reader(csvfile, delimiter=","))
     datareader = datareader[0]
     # this is needed because the list returned is a *nested* list, viz, one that is [[ <blah, blah, blah> ]], and just trying to access it directly means you get the *one* element of the original list. Using [0] effectively strips out the extra brackets.
     number = 1
@@ -20,12 +17,18 @@ with open(urls, 'r') as csvfile:
         submission = reddit.submission(url=row)
         submission.comments.replace_more(limit=None)
         comments = submission.comments.list()
-        df_rows = [[comment.author, comment.id, comment.score, comment.body] for comment in comments]
-        df = pd.DataFrame(df_rows, columns=['Author', 'Comment ID', 'Score', 'Body', ])
-        df.to_csv(f'file{number}.csv')
+        df_rows = [
+            [comment.author, comment.id, comment.score, comment.body]
+            for comment in comments
+        ]
+        df = pd.DataFrame(
+            df_rows,
+            columns=[
+                "Author",
+                "Comment ID",
+                "Score",
+                "Body",
+            ],
+        )
+        df.to_csv(f"file{number}.csv")
         number = number + 1
-
- 
-
-
-
